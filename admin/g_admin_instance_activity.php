0f3daf40dbf1c1b2c79ffb9edf530967218541f3
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-07-16 22:52:35 +0000

    Added admin_instance_activity

---
+++ admin/g_admin_instance_activity.php
@@ -0,0 +1,132 @@
+<?php
+
+
+// $Header: /cvsroot/bitweaver/_bit_galaxia/admin/g_admin_instance_activity.php,v 1.1.2.1 2005/07/16 22:52:35 wolff_borg Exp $
+
+// Copyright (c) 2002-2005, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
+// All Rights Reserved. See copyright.txt for details and a complete list of authors.
+// Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
+// This file was created by Víctor Codocedo on 01/31/2005
+
+require_once( '../../bit_setup_inc.php' );
+
+include_once( GALAXIA_PKG_PATH.'ProcessManager.php');
+include_once( GALAXIA_PKG_PATH.'API.php');
+
+$gBitSystem->verifyPackage( 'galaxia' );
+$gBitSystem->verifyPermission( 'bit_p_admin_workflow', "Permission denied you cannot admin the workflow" );
+
+
+if (!isset($_REQUEST['iid'])) {
+	galaxia_show_error(tra("No instance indicated"));
+	die;
+}
+
+if (!isset($_REQUEST['aid'])) {
+	galaxia_show_error(tra("No activity indicated"));
+}
+
+$smarty->assign('iid', $_REQUEST['iid']);
+$smarty->assign('aid', $_REQUEST['aid']);
+
+// Get workitems and list the workitems with an option to edit workitems for
+// this instance
+if (isset($_REQUEST['save'])) {
+
+	//status, user
+	$instanceManager->set_instance_status($_REQUEST['iid'], $_REQUEST['status']);
+	$instanceManager->set_instance_name($_REQUEST['iid'],$_REQUEST['name']);
+	$instanceManager->set_instance_user($_REQUEST['iid'], $_REQUEST['user']);
+	//y luego acts[activity_id][user] para reasignar users
+	if (isset($_REQUEST['acts'])) {
+		foreach (array_keys($_REQUEST['acts'])as $act) {
+		$instanceManager->set_instance_user($_REQUEST['iid'], $act, $_REQUEST['acts'][$act]);
+		}
+	}
+
+	if ($_REQUEST['sendto']) {
+		$instanceManager->set_instance_destination($_REQUEST['iid'], $_REQUEST['sendto']);
+	}
+//process sendto
+	$instanceManager->set_instance_user($_REQUEST['iid'],$_REQUEST['aid'],$_REQUEST['user_id']);
+}
+
+// Get the instance and set instance information
+$ins_info = $instanceManager->get_instance($_REQUEST['iid']);
+$smarty->assign_by_ref('ins_info', $ins_info);
+
+// Get the process from the instance and set information
+$proc_info = $processManager->get_process($ins_info['p_id']);
+$smarty->assign_by_ref('proc_info', $proc_info);
+
+// Process activities
+$activities = $activityManager->list_activities($ins_info['p_id'], 0, -1, 'flow_num_asc', '', '');
+$smarty->assign('activities', $activities['data']);
+
+// Users
+$users = $gBitUser->get_users(0, -1, 'login_asc', '');
+$smarty->assign_by_ref('users', $users['data']);
+
+$props = $instanceManager->get_instance_properties($_REQUEST['iid']);
+
+if (isset($_REQUEST['unsetprop'])) {
+
+	unset ($props[$_REQUEST['unsetprop']]);
+
+	$instanceManager->set_instance_properties($_REQUEST['iid'], $props);
+}
+
+if (!is_array($props))
+	$props = array();
+
+$smarty->assign_by_ref('props', $props);
+
+if (isset($_REQUEST['addprop'])) {
+	
+	$props[$_REQUEST['name']] = $_REQUEST['value'];
+
+	$instanceManager->set_instance_properties($_REQUEST['iid'], $props);
+}
+
+if (isset($_REQUEST['saveprops'])) {
+	
+	foreach (array_keys($_REQUEST['props'])as $key) {
+		$props[$key] = $_REQUEST['props'][$key];
+	}
+
+	$instanceManager->set_instance_properties($_REQUEST['iid'], $props);
+}
+
+$acts = $instanceManager->get_instance_activities($_REQUEST['iid'],$_REQUEST['aid']);
+$smarty->assign_by_ref('acts', $acts);
+
+$instance->getInstance($_REQUEST['iid']);
+
+$user_id = $gBitUser->getUserId();
+// Process comments
+if (isset($_REQUEST['__removecomment'])) {
+
+	$__comment = $instance->get_instance_comment($_REQUEST['__removecomment']);
+
+	if ($__comment['user_id'] == $user_id or $gBitUser->hasPermission('bit_p_admin_workflow')) {
+		$instance->remove_instance_comment($_REQUEST['__removecomment']);
+	}
+}
+
+$smarty->assign_by_ref('__comments', $__comments);
+
+if (!isset($_REQUEST['__cid']))
+	$_REQUEST['__cid'] = 0;
+
+if (isset($_REQUEST['__post'])) {
+
+	$instance->replace_instance_comment($_REQUEST['__cid'], $_REQUEST['aid'], '', $user_id, $_REQUEST['__title'], $_REQUEST['__comment']);
+}
+
+$__comments = $instance->get_instance_comments($_REQUEST['aid']);
+$smarty->assign('comments',$__comments);
+
+
+$gBitSystem->display( 'bitpackage:Galaxia/g_admin_instance_activity.tpl', tra("Admin Instance Activity") );
+
+?>
---
+++ templates/g_admin_instance_activity.tpl
@@ -0,0 +1,105 @@
+{popup_init src="`$gBitLoc.THEMES_PKG_URL`js/overlib.js"}
+<div class="floaticon">{bithelp}</div>
+
+<div class="admin workflow">
+<div class="header">
+<h1>{tr}Admin instance activity{/tr}</h1>
+</div>
+
+{include file="bitpackage:Galaxia/monitor_nav.tpl"}
+<h3>{tr}Instance Name{/tr}: {$ins_info.name} {$ins_info.instance_id} (Process: {$proc_info.name} {$proc_info.version})</h3>
+<form action="{$gBitLoc.GALAXIA_PKG_URL}admin/g_admin_instance_activity.php" method="post">
+<input type="hidden" name="iid" value="{$iid|escape}" />
+<input type="hidden" name="aid" value="{$aid|escape}" />
+<table class="panel">
+<tr>
+	<td>{tr}Activity{/tr}</td>
+	<td><a href="{$gBitLoc.GALAXIA_PKG_URL}g_run_activity.php?iid={$iid}&amp;activity_id={$aid}">{$acts.name} {if $acts.actstatus eq 'running'}{biticon ipackage="Galaxia" iname="next" iexplain="run activity" iclass="icon"}</a>{/if}</td>
+</tr>
+<tr>
+	<td>{tr}Instance{/tr}</td>
+	<td>{if $acts.actstatus eq 'running'}<input type="text" name="name" value="{$ins_info.name}">{else}{$ins_info.name}{/if}</td>
+</tr>
+<tr>
+	<td>{tr}Created{/tr}</td>
+	<td>{$acts.ia_started|bit_long_datetime</td>
+</tr>
+<!--<tr>
+	<td>{tr}Expiration Date{/tr}</td>
+	<td>{if $acts.exptime eq 0 && $acts.type eq 'activity' && $acts.isInteractive eq 'y'}{tr}Not Defined{/tr}{elseif $acts.type != 'activity'}&lt;{$acts.type}&gt;{elseif $acts.isInteractive eq 'n'}{tr}Not Interactive{/tr}{else}{$acts.exptime|bit_long_datetime}{/if}</td>
+</tr>-->
+<tr>
+	<td>{tr}Ended{/tr}</td>
+	<td>{if $acts.ended eq 0}-{else}{$acts.ended|bit_long_datetime{/if}</td>
+<tr>
+	<td>{tr}Status{/tr}</td>
+	<td>{tr}{$acts.actstatus}{/tr}</td>
+</tr>
+<tr>
+	<td>{tr}User{/tr}</td>
+	<td>
+	{if $acts.actstatus eq 'running'}
+	<select name="owner">
+	{section name=ix loop=$users}
+	<option value="{$users[ix].user_id|escape}" {if $users[ix].user_id eq $acts.user_id}selected="selected"{/if}>{displayname user_id=$users[ix].user_id}</option>
+	{/section}
+	</select>
+	{else}
+	{displayname user_id=$acts.user_id}
+	{/if}
+	</td>
+</tr>
+<tr class="panelsubmitrow"><td colspan="2">
+	{if $acts.actstatus eq 'running'}<input type="submit" name="save" value="{tr}update{/tr}" />{/if}
+</td></tr>
+</table>
+</form>
+
+<h3>{tr}Comments{/tr}</h3>
+{section name=ix loop=$comments}
+<table class="box">
+<tr>
+    	<td>{tr}From{/tr}:</td>
+	<td>{displayname user_id=$comments[ix].user_id}</td>
+</tr><tr>
+    	<td>{tr}Date{/tr}:</td>
+	<td colspan="3">{$comments[ix].timestamp|bit_long_datetime</td>
+</tr><tr>
+	<td>{tr}Subject{/tr}:</td>
+	<td colspan="3">{$comments[ix].title}</td>
+</tr><tr>
+	<td>{tr}Body{/tr}:</td>
+	<td colspan="3">{$comments[ix].comment}</td>
+</tr><tr>
+	<td class="panelsubmitrow">
+	<form action="{$gBitLoc.GALAXIA_PKG_URL}admin/g_admin_instance_activity.php" method="post">
+		<input type="submit" name="eraser" value="{tr}erase{/tr}" />
+		<input type="hidden" name="__removecomment" value="{$comments[ix].cId}" />
+		<input type="hidden" name="iid" value="{$iid|escape}" />
+		<input type="hidden" name="aid" value="{$aid|escape}" />
+	</form></td>
+</tr>
+</table>
+{/section}
+
+<h3>{tr}Answer{/tr}:</h3>
+<form action="{$gBitLoc.GALAXIA_PKG_URL}admin/g_admin_instance_activity.php" method="post">
+<input type="hidden" name="iid" value="{$iid|escape}" />
+<input type="hidden" name="aid" value="{$aid|escape}" />
+<table class="panel">
+<tr>
+	<td>{tr}Subject{/tr}:</td>
+	<td><input type="text" name="__title"></td>
+</tr>
+<tr>
+	<td>{tr}Body{/tr}:</td>
+	<td><textarea rows="5" cols="60" name="__comment"></textarea></td>
+</tr>
+<tr class="panelsubmitrow"><td colspan="2">
+	<input type="submit" name="answer" value="{tr}Save{/tr}"/></td>
+</tr>
+</table>
+<input type="hidden" name="__post" value="y" />
+</form>
+</div>
+</div>
