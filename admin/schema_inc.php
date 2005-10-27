882fb965754f4511544964d9d1dbceffe3f1cd9f
Max Kremmel <xing@synapse.plus.com>
2005-10-27 14:18:28 +0000

    "cleaned up all schema_inc.php files - used <pkg>_PKG_NAME instead of '<pkg>' and moved registerSchemaDefaults() to registerPreferences() and registerUserPermission() where appropriate"

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -4,98 +4,98 @@ include_once( GALAXIA_PKG_PATH.'config.php' );
 $tables = array(
 
 'galaxia_activities' => "
-  activity_id I4 AUTO PRIMARY,
-  name C(80),
-  normalized_name C(80),
-  p_id I4 NOTNULL,
-  type C(12) NOTNULL,
-  is_auto_routed C(1),
-  flow_num I8,
-  is_interactive C(1),
-  expiration_time I8 DEFAULT 0,
-  last_modified I8,
-  description X
+	activity_id I4 AUTO PRIMARY,
+	name C(80),
+	normalized_name C(80),
+	p_id I4 NOTNULL,
+	type C(12) NOTNULL,
+	is_auto_routed C(1),
+	flow_num I8,
+	is_interactive C(1),
+	expiration_time I8 DEFAULT 0,
+	last_modified I8,
+	description X
 ",
 
 'galaxia_activity_roles' => "
-  activity_id I4 PRIMARY,
-  role_id I4 PRIMARY
+	activity_id I4 PRIMARY,
+	role_id I4 PRIMARY
 ",
 
 'galaxia_instance_activities' => "
-  instance_id I4 PRIMARY,
-  activity_id I4 PRIMARY,
-  started I8 NOTNULL,
-  ended I8 NOTNULL,
-  user_id I4,
-  status C(12) NOTNULL
+	instance_id I4 PRIMARY,
+	activity_id I4 PRIMARY,
+	started I8 NOTNULL,
+	ended I8 NOTNULL,
+	user_id I4,
+	status C(12) NOTNULL
 ",
 
 'galaxia_instance_comments' => "
-  c_id I4 AUTO PRIMARY,
-  instance_id I4 NOTNULL,
-  user_id I4,
-  activity_id I4,
-  hash C(32),
-  title C(250),
-  comment X,
-  activity C(80),
-  timestamp I8
+	c_id I4 AUTO PRIMARY,
+	instance_id I4 NOTNULL,
+	user_id I4,
+	activity_id I4,
+	hash C(32),
+	title C(250),
+	comment X,
+	activity C(80),
+	timestamp I8
 ",
 
 'galaxia_instances' => "
-  instance_id I4 AUTO PRIMARY,
-  name C(80),
-  p_id I4 NOTNULL,
-  started I8,
-  owner_id I4,
-  next_activity I8,
-  next_group_id I4,
-  ended I8,
-  status C(12) NOTNULL,
-  properties X
+	instance_id I4 AUTO PRIMARY,
+	name C(80),
+	p_id I4 NOTNULL,
+	started I8,
+	owner_id I4,
+	next_activity I8,
+	next_group_id I4,
+	ended I8,
+	status C(12) NOTNULL,
+	properties X
 ",
 
 'galaxia_processes' => "
-  p_id I4 AUTO PRIMARY,
-  procname C(80),
-  is_valid C(1),
-  is_active C(1),
-  version C(12),
-  description X,
-  last_modified I8,
-  normalized_name C(80)
+	p_id I4 AUTO PRIMARY,
+	procname C(80),
+	is_valid C(1),
+	is_active C(1),
+	version C(12),
+	description X,
+	last_modified I8,
+	normalized_name C(80)
 ",
 
 'galaxia_roles' => "
-  role_id I4 AUTO PRIMARY,
-  p_id I4 NOT NULL,
-  last_modified I8,
-  name C(80),
-  description X
+	role_id I4 AUTO PRIMARY,
+	p_id I4 NOT NULL,
+	last_modified I8,
+	name C(80),
+	description X
 ",
 
 'galaxia_transitions' => "
-  p_id I4 NOTNULL,
-  act_from_id I4 PRIMARY,
-  act_to_id I4 PRIMARY
+	p_id I4 NOTNULL,
+	act_from_id I4 PRIMARY,
+	act_to_id I4 PRIMARY
 ",
 
 'galaxia_group_roles' => "
-  p_id I4 NOT NULL,
-  role_id I4 AUTO PRIMARY,
-  group_id I4 PRIMARY
+	p_id I4 NOT NULL,
+	role_id I4 AUTO PRIMARY,
+	group_id I4 PRIMARY
 ",
 
 'galaxia_workitems' => "
-  item_id I4 AUTO PRIMARY,
-  instance_id I4 NOTNULL,
-  order_id I4 NOTNULL,
-  activity_id I4 NOTNULL,
-  properties X,
-  started I8,
-  ended I8,
-  user_id I4
+	item_id I4 AUTO PRIMARY,
+	instance_id I4 NOTNULL,
+	order_id I4 NOTNULL,
+	activity_id I4 NOTNULL,
+	properties X,
+	started I8,
+	ended I8,
+	user_id I4
 "
 
 );
