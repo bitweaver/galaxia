f850b1b9eac758087c6063091f2cabdc10242f1a
Max Kremmel <xing@synapse.plus.com>
2006-01-08 08:57:50 +0000

    simplify admin code

--- admin/admin_galaxia_inc.php
+++ admin/admin_galaxia_inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_galaxia/admin/admin_galaxia_inc.php,v 1.1.2.1 2005/11/24 10:47:18 wolff_borg Exp $
+// $Header: /cvsroot/bitweaver/_bit_galaxia/admin/admin_galaxia_inc.php,v 1.1.2.2 2006/01/08 08:57:50 squareing Exp $
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -12,11 +12,8 @@ $formGalaxia = array(
 );
 $gBitSmarty->assign( 'formGalaxia', $formGalaxia );
 
-$processForm = set_tab();
-
-if( $processForm ) {
-	$blogToggles = array_merge( $formGalaxia);
-	foreach( $blogToggles as $item => $data ) {
+if( !empty( $_REQUEST['list_submit'] ) ) {
+	foreach( $formGalaxia as $item => $data ) {
 		simple_set_toggle( $item );
 	}
 }
