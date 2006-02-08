2837bbc0c4e1ba14dc206d08c674e5661469e127
Max Kremmel <xing@synapse.plus.com>
2006-02-08 21:51:13 +0000

    php: introduce new method: getPref( $package, $pref, $defaut ) (order is open for discussion tpl: $gBitPref.package.pref applied package name to all saves - should make database more organised and should allow more reliable uninstalls in the future

--- admin/admin_galaxia_inc.php
+++ admin/admin_galaxia_inc.php
@@ -1,5 +1,5 @@
 <?php
-// $Header: /cvsroot/bitweaver/_bit_galaxia/admin/admin_galaxia_inc.php,v 1.3 2006/01/10 21:11:59 squareing Exp $
+// $Header: /cvsroot/bitweaver/_bit_galaxia/admin/admin_galaxia_inc.php,v 1.4 2006/02/08 21:51:13 squareing Exp $
 // Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
 // All Rights Reserved. See copyright.txt for details and a complete list of authors.
 // Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details.
@@ -14,7 +14,7 @@ $gBitSmarty->assign( 'formGalaxia', $formGalaxia );
 
 if( !empty( $_REQUEST['list_submit'] ) ) {
 	foreach( $formGalaxia as $item => $data ) {
-		simple_set_toggle( $item );
+		simple_set_toggle( $item, GALAXIA_PKG_NAME );
 	}
 }
 
