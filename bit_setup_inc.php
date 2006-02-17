4683dd32ce389975559e46d79ea527fdfef5eab7
Max Kremmel <xing@synapse.plus.com>
2006-02-17 23:51:41 +0000

    registerPackage now takes a hash instead of multiple parameters - keeps # of parameters under control

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,22 +1,27 @@
 <?php
-	global $gBitSystem;
-	$gBitSystem->registerPackage( 'galaxia', dirname( __FILE__).'/' );
+global $gBitSystem;
 
-	if( $gBitSystem->isPackageActive( 'galaxia' ) ) {
-		if ($gBitUser->hasPermission( 'bit_p_use_workflow' ))
-			$gBitSystem->registerAppMenu( GALAXIA_PKG_NAME, 'Workflow', GALAXIA_PKG_URL.'g_user_processes.php', 'bitpackage:Galaxia/menu_galaxia.tpl' );
+$registerHash = array(
+	'package_name' => 'galaxia',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
-		// **********  GALAXIA  ************
+if( $gBitSystem->isPackageActive( 'galaxia' ) ) {
+	if ($gBitUser->hasPermission( 'bit_p_use_workflow' ))
+		$gBitSystem->registerAppMenu( GALAXIA_PKG_NAME, 'Workflow', GALAXIA_PKG_URL.'g_user_processes.php', 'bitpackage:Galaxia/menu_galaxia.tpl' );
+
+	// **********  GALAXIA  ************
 /*		if ($bit_p_admin_workflow == 'y')
+	{
+		$perms = $userlib->get_permissions(0, -1, 'perm_name_desc', '', 'workflow');
+		foreach ($perms["data"] as $perm)
 		{
-			$perms = $userlib->get_permissions(0, -1, 'perm_name_desc', '', 'workflow');
-			foreach ($perms["data"] as $perm)
-			{
-				$perm = $perm["perm_name"];
-				$gBitSmarty->assign("$perm", 'y');
-				$$perm = 'y';
-			}
+			$perm = $perm["perm_name"];
+			$gBitSmarty->assign("$perm", 'y');
+			$$perm = 'y';
 		}
-*/
 	}
+*/
+}
 ?>
