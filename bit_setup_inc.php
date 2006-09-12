96cfef843a35946105bce9db425af490cff2c1a8
Max Kremmel <xing@synapse.plus.com>
2006-09-12 20:02:07 +0000

    clean up registerAppMenu API, clean up a lot of the menu code, don't assign appMenu to templates but use $gBitSystem->mAppMenu directly

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -9,12 +9,19 @@ $registerHash = array(
 $gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'galaxia' ) ) {
-	if ($gBitUser->hasPermission( 'p_galaxia_use' ))
+	if( $gBitUser->hasPermission( 'p_galaxia_use' ) ) {
+		$menuHash = array(
+			'package_name'  => GALAXIA_PKG_NAME,
+			'index_url'     => GALAXIA_PKG_URL.'g_user_processes.php',
+			'menu_template' => 'bitpackage:GALAXIA/menu_GALAXIA.tpl',
+		);
+		$gBitSystem->registerAppMenu( $menuHash );
 		$gBitSystem->registerAppMenu( GALAXIA_PKG_NAME, 'Workflow', GALAXIA_PKG_URL.'g_user_processes.php', 'bitpackage:Galaxia/menu_galaxia.tpl' );
+	}
 
+/*
 	// **********  GALAXIA  ************
-/*		if ($p_galaxia_admin == 'y')
-	{
+	if ($p_galaxia_admin == 'y') {
 		$perms = $userlib->get_permissions(0, -1, 'perm_name_desc', '', 'workflow');
 		foreach ($perms["data"] as $perm)
 		{
